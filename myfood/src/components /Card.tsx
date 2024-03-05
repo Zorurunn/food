c135d6114f3e4b5b91f1128b3ba82058ba62c632
Zolzaya Udval <23LP6080@G301-CC0198.local>
2024-03-05 12:25:10 +0800

    qq

--- myfood/src/components /Card.tsx
+++ myfood/src/components /Card.tsx
@@ -10,7 +10,7 @@ export const Card = (props: foodType) => {
   const { setOpenFoodDetail, setOpenEditFood } = useBackDrop();
   const { imgPath, price, name, discount } = props;
   const [isHover, setIsHover] = useState(false);
-  const [isAdmin, setIsAdmin] = useState(true);
+  const [isAdmin, setIsAdmin] = useState(false);
 
   return (
     <Stack
