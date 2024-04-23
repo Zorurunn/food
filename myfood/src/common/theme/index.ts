2ffe1ca7538d83df0a932ed7e1e78c269dbda1a3
Zolzaya Udval <23LP6080@G303-CC0143.local>
2024-04-23 08:34:53 +0800

    theme color changed

--- myfood/src/common/theme/index.ts
+++ myfood/src/common/theme/index.ts
@@ -4,9 +4,9 @@ import { createTheme } from "@mui/material/styles";
 export const theme = createTheme({
   palette: {
     primary: {
-      main: "#12A795",
-      light: "#C1E6CF",
-      dark: "#B7DDFF",
+      main: "#18BA51",
+      light: "#F7F7F8",
+      dark: "#F7F7F8",
       contrastText: "#18BA5133",
     },
   },
